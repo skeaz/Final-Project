7ddebc7f4c6f86460d2355dc8afa37a236170806
husseinshaheed <shaheey33@gmail.com>
2023-09-05 09:11:06 +0300

    add footer and add some @media in css

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const slides = document.querySelectorAll(".slide");
+
 let currentSlide = 0;
 let autoSlideInterval;
 
@@ -6,7 +7,7 @@ function showSlide(index) {
   slides[currentSlide].style.display = "none";
   slides[index].style.display = "block";
   $(".slide").hide(); // أخفي جميع الشرائح
-  $(".slide").eq(index).fadeIn(500); // عرض الشريحة المحددة بانتقال
+  $(".slide").eq(index).fadeIn(100); // عرض الشريحة المحددة بانتقال
 
   currentSlide = index;
 }
@@ -43,17 +44,30 @@ prevBtn.addEventListener("click", () => {
   showSlide(newIndex);
   startAutoSlide();
 });
-window.onscroll = function () {
-  myFunction();
-};
+// window.onscroll = function () {
+//   myFunction();
+// };
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    navbar.classList.add("sticky");
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     navbar.classList.add("sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+let dropbtn = document.getElementById("dropbtn");
+let dropdown = document.getElementById("dropdown-content2");
+let isOpen = false;
+
+dropbtn.addEventListener("click", () => {
+  if (isOpen) {
+    dropdown.style.display = "none"; // Close the dropdown
   } else {
-    navbar.classList.remove("sticky");
+    dropdown.style.display = "block"; // Open the dropdown
   }
-}
+  isOpen = !isOpen; // Toggle the isOpen flag
+});
+
