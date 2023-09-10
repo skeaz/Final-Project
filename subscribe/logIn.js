0794bda66880bf687bd9997ceabb515216b23426
husseinshaheed <shaheey33@gmail.com>
2023-09-10 19:32:36 +0300

    add some page with ui/ux + add product page

--- subscribe/logIn.js
+++ subscribe/logIn.js
@@ -1,11 +1,20 @@
-const signUpButton = document.getElementById("signUp");
-const signInButton = document.getElementById("signIn");
+const signUpToggle = document.getElementById("signUp");
+const signInToggle = document.getElementById("signIn");
 const container = document.getElementById("container");
 
-signUpButton.addEventListener("click", () => {
+signUpToggle.addEventListener("click", () => {
   container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener("click", () => {
+signInToggle.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
+});
+const signInButton = document.getElementById("signInBut");
+const signUpButton = document.getElementById("signUpBut");
+const emailSignIn = document.getElementById("emailSignIn");
+const passwordSignIn = document.getElementById("passwordSignIn");
+signInButton.addEventListener("click", () => {
+  if (emailSignIn.value === "admin" && passwordSignIn.value === "admin") {
+    window.location.href = "../admin/admin.html"; // Replace with your desired URL
+  }
 });
